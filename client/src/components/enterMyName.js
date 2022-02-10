de6dde1a1a077c569bd5cfcbf190cafb634a4af3
Glor147g <gloria_lilley@yahoo.co.uk>
2022-02-10 22:33:14 +0000

    added class and style to entermyfirstname form, created styles folder

--- client/src/components/enterMyName.js
+++ client/src/components/enterMyName.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import "../styles/app.css"
 
 const enterMyFirstName =()=> {
 return (
-<form>
+<form className = "enterMyFirstName" >
 <label for ="uname"><b>Readers Name</b> </label>
 <input type= "text"/>
 <button type="submit"> OK </button>
