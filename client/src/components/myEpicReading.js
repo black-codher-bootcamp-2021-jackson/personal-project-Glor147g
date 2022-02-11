deec418942ffb085d722020e038e6ed99c4513e7
Glor147g <gloria_lilley@yahoo.co.uk>
2022-02-11 11:11:02 +0000

    Home page: added title and bottom page items

--- client/src/components/myEpicReading.js
+++ client/src/components/myEpicReading.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import '.Styles/App.css';
 
 const myEpicReading =()=> {
-return
+return (
+	<div className='title'>
+	<h2> Welcome to My Epic Reading! </h2>
+	<ul className='PracticeAboutLogin'>
+
+		<li>Practice </li>
+		<li>About </li>
+		<li>Login </li>
+	</ul>
+	</div>
+
+
+)
 
 }
 
