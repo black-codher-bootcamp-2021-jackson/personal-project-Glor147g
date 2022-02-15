6de4108787d070a80d03cf6cefae1840091e385d
Glor147g <gloria_lilley@yahoo.co.uk>
2022-02-15 20:06:10 +0000

    updated profile schema with {}

--- server/models/Profiles.js
+++ server/models/Profiles.js
@@ -2,13 +2,24 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const profileSchema = new Schema({
-  first_name: String,
-  Age: Number,
-  Image: String, //it will be an imageURL String
-  books: ["bookID"], //connect/match the id
-  numberOfBooksRead: Number,
-  lengthOfTimeSpent: Number,
-  numberOfDaysSpentInARow: Number,
+  name: {
+    type: String,
+    required: true,
+  },
+
+  Age: {
+    type: Number,
+    required: true,
+  },
+  Avatar: {
+    type: String,
+    required: true,
+  }
+  //it will be an imageURL String
+  // books: ["bookID"], //connect/match the id
+  // numberOfBooksRead: Number,
+  // lengthOfTimeSpent: Number,
+  // numberOfDaysSpentInARow: Number,
 });
 //
 mongoose.model("profiles", profileSchema);
