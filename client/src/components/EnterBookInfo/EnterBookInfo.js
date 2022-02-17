53681a8288ac4e064b297535e3c200ec49c3edc7
Glor147g <gloria_lilley@yahoo.co.uk>
2022-02-17 00:10:15 +0000

    updated form and css

--- client/src/components/EnterBookInfo/EnterBookInfo.js
+++ client/src/components/EnterBookInfo/EnterBookInfo.js
@@ -2,58 +2,95 @@ import React from "react";
 import "./EnterBookInfo.css";
 import {useState} from "react";
 
+
+
+
 const EnterBookInfo = () => {
-    const [inputs, setInputs] = useState ({});
-
-    const handleChange = (event) => {
-     const name = event.target.name;
-     const value = event.target.value;
-     setInputs(values => ({...values, [name]: value}))
-    }
-
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      // alert('inputs')
-    }
+    const [title, setTitle] = useState ('');
 
     return (
-      <div className="form">
+      <div className="create">
       <h2> Enter Book Information </h2>
-      
-      <form onSubmit={handleSubmit}>
-        <label> Book Name
+      <form>
+        <label> Book Name: </label>
           <input 
           type="text"
-          name="title"
-          value={inputs.title || ""} 
-          onChange={handleChange}
+          required
+          value={title} 
+        
           />
-        </label>
-        <div>
-        <label> Authors
+           <label> Author(s): </label>
           <input 
           type="text"
-          name="authors"
-          value={inputs.authors || ""} 
-          onChange={handleChange}
-          />
-          </label>
-          </div>
-
-          <div>
-        <label> Pages(s)
+          required
+          value={title} 
+        />
+           <label> page(s): </label>
           <input 
-          type="number"
-          name="pages"
-          value={inputs.pages || ""} 
-          onChange={handleChange}
-          />
-          </label>
+          type="text"
+          required
+          value={title} 
+        />
+        <button> Add Book </button>
+          </form>
           </div>
-        <input type="submit" value="ok" />
-      </form>
-    // </div>
   );
 };
 
+
 export default EnterBookInfo;
+
+// const EnterBookInfo = () => {
+//     const [inputs, setInputs] = useState ({});
+
+//     const handleChange = (event) => {
+//      const name = event.target.name;
+//      const value = event.target.value;
+//      setInputs(values => ({...values, [name]: value}))
+//     }
+
+//     const handleSubmit = (event) => {
+//       event.preventDefault();
+//       // alert('inputs')
+//     }
+
+//     return (
+//       <div className="form">
+//       <h2> Enter Book Information </h2>
+//       <form onSubmit={handleSubmit}>
+//         <label> Book Name
+//           <input 
+//           type="text"
+//           name="title"
+//           value={inputs.title || ""} 
+//           onChange={handleChange}
+//           />
+//         </label>
+//         <div>
+//         <label> Authors
+//           <input 
+//           type="text"
+//           name="authors"
+//           value={inputs.authors || ""} 
+//           onChange={handleChange}
+//           />
+//           </label>
+//           </div>
+
+//           <div>
+//         <label> Pages(s)
+//           <input 
+//           type="number"
+//           name="pages"
+//           value={inputs.pages || ""} 
+//           onChange={handleChange}
+//           />
+//           </label>
+//           </div>
+//         <input type="submit" value="ok" />
+        
+//       </form>
+//     </div>
+//   );
+// };
+
