8a14daf7bace99b21d6b74931630a866fae7a94e
Glor147g <gloria_lilley@yahoo.co.uk>
2022-02-19 00:15:08 +0000

    rainbow navbar

--- client/src/components/NavBar/Navbar.js
+++ client/src/components/NavBar/Navbar.js
@@ -5,12 +5,15 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <h1> The Epic Reader App</h1>
-      <div className="links">
+      <div className="links-1">
         <a href="/">Home</a>
+        <div className="links-2">
         <a href="/About">About</a>
+        <div className="links-3">
         <a href="/Testimonials">Testimonials </a>
       </div>
-      
+      </div>
+      </div>
     </nav>
   );
 };
