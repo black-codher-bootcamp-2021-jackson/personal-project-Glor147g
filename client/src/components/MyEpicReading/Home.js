155e20452d4660132ea5c45d1ad78401a6cd5cd3
Glor147g <gloria_lilley@yahoo.co.uk>
2022-02-19 00:41:08 +0000

    rainbow home page links

--- client/src/components/MyEpicReading/Home.js
+++ client/src/components/MyEpicReading/Home.js
@@ -5,14 +5,20 @@ const Home = () => {
   return (
     <div className="title">
       <h2> "Connecting families through the joy of reading "! </h2>
-
-      
+      <div className="links-1">
+        <a href="/Start Reading">Start Reading</a>
+      <div className="links-2">
+        <a href="/Amaia-Grace">Amaia-Grace</a>
+      <div className="links-3">
+        <a href="/Kai">Kai</a>
         {/* <button on > Add A reader</button>
         <Link to="/search"> ok </Link> */}
-        <div className=" create">
-         <button>Start Reading </button> 
-        <button> Amaia-Grace</button> 
-       <button> Kai </button>
+        {/* <div className="create">
+         {/* <button className="S"> </button>  */}
+        {/* <button className="A"> Amaia-Grace</button> 
+       <button className="K"> Kai </button> */} *
+        </div>
+        </div>
         </div>
         </div>
       
